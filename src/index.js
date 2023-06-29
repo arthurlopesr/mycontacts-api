cd9cba15a9530ff0ad497c95da74b79c816c5bb4
Arthur Lopes <arthurlopr12@gmail.com>
2023-06-29 20:29:22 -0300

    eslint config / editor config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello world');
-})
+  response.send('Hello world');
+});
 
 app.listen(3000, () => console.log('🔥 Server started att http://localhost:3000'));
