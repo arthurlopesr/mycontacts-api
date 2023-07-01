fb825935920ed368e995670bb074cba6c04b3a60
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-01 11:08:52 -0300

    Catefory controller

---
+++ src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,44 @@
+const db = require('../../database');
+
+class CategoriesRepository {
+  async findAll() {
+    const rows = await db.query('SELECT * FROM categories ORDER BY name');
+    return rows;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(`
+    INSERT INTO categories(name)
+    VALUES($1)
+    RETURNING *
+    `, [name]);
+    return row;
+  }
+
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM categories WHERE id = $1', [id]);
+    return row;
+  }
+
+  async findByName(name) {
+    const [row] = await db.query('SELECT * FROM categories WHERE name = $1', [name]);
+    return row;
+  }
+
+  async update(id, { name }) {
+    const row = db.query(`
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `, [name, id]);
+    return row;
+  }
+
+  async delete(id) {
+    const deleteCategory = await db.query('DELETE FROM categories WHERE id = $1', [id]);
+    return deleteCategory;
+  }
+}
+
+module.exports = new CategoriesRepository();
